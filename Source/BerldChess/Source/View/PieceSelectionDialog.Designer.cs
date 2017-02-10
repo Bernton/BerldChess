f809c338f199b7369d819bd195ff850cc6b88a0c
Bernton <nielsen.bernt@hotmail.com>
2017-02-10 17:09:17 +0100

    Add PieceSelection files

---
+++ Source/BerldChess/Source/View/PieceSelectionDialog.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace BerldChess.View
+{
+    partial class PieceSelectionDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PieceSelectionDialog));
+            this._checkBoxUnicodeFont = new System.Windows.Forms.CheckBox();
+            this._buttonApply = new System.Windows.Forms.Button();
+            this._comboBoxFont = new System.Windows.Forms.ComboBox();
+            this._checkBoxDefault = new System.Windows.Forms.CheckBox();
+            this._buttonPreview = new System.Windows.Forms.Button();
+            this._textBoxFontChars = new System.Windows.Forms.TextBox();
+            this._labelFontChars = new System.Windows.Forms.Label();
+            this._textBoxSizeFactor = new System.Windows.Forms.TextBox();
+            this._labelSize = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _checkBoxUnicodeFont
+            // 
+            this._checkBoxUnicodeFont.AutoSize = true;
+            this._checkBoxUnicodeFont.Location = new System.Drawing.Point(12, 80);
+            this._checkBoxUnicodeFont.Name = "_checkBoxUnicodeFont";
+            this._checkBoxUnicodeFont.Size = new System.Drawing.Size(101, 17);
+            this._checkBoxUnicodeFont.TabIndex = 0;
+            this._checkBoxUnicodeFont.Text = "Is Unicode Font";
+            this._checkBoxUnicodeFont.UseVisualStyleBackColor = true;
+            this._checkBoxUnicodeFont.CheckedChanged += new System.EventHandler(this.OnCheckBoxUnicodeFontCheckedChanged);
+            // 
+            // _buttonApply
+            // 
+            this._buttonApply.Location = new System.Drawing.Point(141, 189);
+            this._buttonApply.Name = "_buttonApply";
+            this._buttonApply.Size = new System.Drawing.Size(130, 32);
+            this._buttonApply.TabIndex = 2;
+            this._buttonApply.Text = "Apply and Close";
+            this._buttonApply.UseVisualStyleBackColor = true;
+            this._buttonApply.Click += new System.EventHandler(this.OnButtonApplyCloseClick);
+            // 
+            // _comboBoxFont
+            // 
+            this._comboBoxFont.FormattingEnabled = true;
+            this._comboBoxFont.Location = new System.Drawing.Point(12, 53);
+            this._comboBoxFont.Name = "_comboBoxFont";
+            this._comboBoxFont.Size = new System.Drawing.Size(259, 21);
+            this._comboBoxFont.TabIndex = 3;
+            this._comboBoxFont.TextChanged += new System.EventHandler(this.OnComboBoxFontTextChanged);
+            // 
+            // _checkBoxDefault
+            // 
+            this._checkBoxDefault.AutoSize = true;
+            this._checkBoxDefault.Location = new System.Drawing.Point(12, 12);
+            this._checkBoxDefault.Name = "_checkBoxDefault";
+            this._checkBoxDefault.Size = new System.Drawing.Size(82, 17);
+            this._checkBoxDefault.TabIndex = 4;
+            this._checkBoxDefault.Text = "Use Default";
+            this._checkBoxDefault.UseVisualStyleBackColor = true;
+            this._checkBoxDefault.CheckedChanged += new System.EventHandler(this.OnCheckBoxDefaultCheckedChanged);
+            // 
+            // _buttonPreview
+            // 
+            this._buttonPreview.Location = new System.Drawing.Point(12, 189);
+            this._buttonPreview.Name = "_buttonPreview";
+            this._buttonPreview.Size = new System.Drawing.Size(123, 32);
+            this._buttonPreview.TabIndex = 5;
+            this._buttonPreview.Text = "Apply";
+            this._buttonPreview.UseVisualStyleBackColor = true;
+            this._buttonPreview.Click += new System.EventHandler(this.OnButtonApplyClick);
+            // 
+            // _textBoxFontChars
+            // 
+            this._textBoxFontChars.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._textBoxFontChars.Location = new System.Drawing.Point(12, 135);
+            this._textBoxFontChars.Name = "_textBoxFontChars";
+            this._textBoxFontChars.Size = new System.Drawing.Size(259, 35);
+            this._textBoxFontChars.TabIndex = 6;
+            this._textBoxFontChars.Tag = "kqbnrplwvmto";
+            this._textBoxFontChars.Text = "kqbnrplwvmto";
+            // 
+            // _labelFontChars
+            // 
+            this._labelFontChars.AutoSize = true;
+            this._labelFontChars.Location = new System.Drawing.Point(9, 119);
+            this._labelFontChars.Name = "_labelFontChars";
+            this._labelFontChars.Size = new System.Drawing.Size(266, 13);
+            this._labelFontChars.TabIndex = 7;
+            this._labelFontChars.Text = "Chess Font Characters (Order: K,Q,B,N,R,P,k,q,b,n,r,p)";
+            // 
+            // textBox1
+            // 
+            this._textBoxSizeFactor.Location = new System.Drawing.Point(196, 10);
+            this._textBoxSizeFactor.Name = "textBox1";
+            this._textBoxSizeFactor.Size = new System.Drawing.Size(79, 20);
+            this._textBoxSizeFactor.TabIndex = 8;
+            this._textBoxSizeFactor.Text = "0.9";
+            // 
+            // _labelSize
+            // 
+            this._labelSize.AutoSize = true;
+            this._labelSize.Location = new System.Drawing.Point(163, 13);
+            this._labelSize.Name = "_labelSize";
+            this._labelSize.Size = new System.Drawing.Size(30, 13);
+            this._labelSize.TabIndex = 9;
+            this._labelSize.Text = "Size:";
+            // 
+            // PieceSelectionDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 233);
+            this.Controls.Add(this._labelSize);
+            this.Controls.Add(this._textBoxSizeFactor);
+            this.Controls.Add(this._labelFontChars);
+            this.Controls.Add(this._textBoxFontChars);
+            this.Controls.Add(this._buttonPreview);
+            this.Controls.Add(this._checkBoxDefault);
+            this.Controls.Add(this._comboBoxFont);
+            this.Controls.Add(this._buttonApply);
+            this.Controls.Add(this._checkBoxUnicodeFont);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PieceSelectionDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Alter Piece Design";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox _checkBoxUnicodeFont;
+        private System.Windows.Forms.Button _buttonApply;
+        private System.Windows.Forms.ComboBox _comboBoxFont;
+        private System.Windows.Forms.CheckBox _checkBoxDefault;
+        private System.Windows.Forms.Button _buttonPreview;
+        private System.Windows.Forms.TextBox _textBoxFontChars;
+        private System.Windows.Forms.Label _labelFontChars;
+        private System.Windows.Forms.TextBox _textBoxSizeFactor;
+        private System.Windows.Forms.Label _labelSize;
+    }
+}
---
+++ Source/BerldChess/Source/View/PieceSelectionDialog.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+namespace BerldChess.View
+{
+    public partial class PieceSelectionDialog : Form
+    {
+        public event Action FontSelected;
+
+        private string _pieceFontFamily;
+        private bool _isUnicodeFont;
+        private string _chessFontChars;
+        private double _sizeFactor;
+
+        public double SizeFactor
+        {
+            get
+            {
+                return _sizeFactor;
+            }
+            set
+            {
+                _sizeFactor = value;
+                _textBoxSizeFactor.Text = _sizeFactor.ToString();
+            }
+        }
+
+        public string ChessFontChars
+        {
+            get
+            {
+                return _chessFontChars;
+            }
+            set
+            {
+                _chessFontChars = value;
+                _textBoxFontChars.Text = value;
+            }
+        }
+
+        public bool IsUnicodeFont
+        {
+            get
+            {
+                return _isUnicodeFont;
+            }
+
+            set
+            {
+                _isUnicodeFont = value;
+                _checkBoxUnicodeFont.Checked = _isUnicodeFont;
+            }
+        }
+
+        public string PieceFontFamily
+        {
+            get
+            {
+                return _pieceFontFamily;
+            }
+            set
+            {
+                _pieceFontFamily = value;
+                _comboBoxFont.Text = _pieceFontFamily;
+
+                if (_pieceFontFamily == "")
+                {
+                    _checkBoxDefault.Checked = true;
+                }
+                else
+                {
+                    _checkBoxDefault.Checked = false;
+                }
+            }
+        }
+
+        public PieceSelectionDialog()
+        {
+            InitializeComponent();
+
+            InstalledFontCollection fonts = new InstalledFontCollection();
+
+            for (int i = 0; i < fonts.Families.Length; i++)
+            {
+                _comboBoxFont.Items.Add(fonts.Families[i].Name);
+            }
+        }
+
+        private void OnButtonApplyCloseClick(object sender, EventArgs e)
+        {
+            if (_checkBoxDefault.Checked)
+            {
+                PieceFontFamily = "";
+                DialogResult = DialogResult.OK;
+            }
+            else if (DoesFontExist(_comboBoxFont.Text, FontStyle.Regular))
+            {
+                double factor;
+
+                if(double.TryParse(_textBoxSizeFactor.Text, out factor))
+                {
+                    _sizeFactor = factor;
+                }
+
+                _isUnicodeFont = _checkBoxUnicodeFont.Checked;
+                PieceFontFamily = _comboBoxFont.Text;
+                ChessFontChars = _textBoxFontChars.Text;
+                DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                MessageBox.Show("Invalid font.", "Berd Chess", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void OnButtonApplyClick(object sender, EventArgs e)
+        {
+            if (_checkBoxDefault.Checked)
+            {
+                PieceFontFamily = "";
+                FontSelected?.Invoke();
+            }
+            else if (DoesFontExist(_comboBoxFont.Text, FontStyle.Regular))
+            {
+                double factor;
+
+                if (double.TryParse(_textBoxSizeFactor.Text, out factor))
+                {
+                    _sizeFactor = factor;
+                }
+
+                _isUnicodeFont = _checkBoxUnicodeFont.Checked;
+                PieceFontFamily = _comboBoxFont.Text;
+                ChessFontChars = _textBoxFontChars.Text;
+                FontSelected?.Invoke();
+            }
+        }
+
+        private bool DoesFontExist(string fontFamilyName, FontStyle fontStyle)
+        {
+            bool result;
+
+            try
+            {
+                using (FontFamily family = new FontFamily(fontFamilyName))
+                    result = family.IsStyleAvailable(fontStyle);
+            }
+            catch (ArgumentException)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        private void OnCheckBoxDefaultCheckedChanged(object sender, EventArgs e)
+        {
+            if (_checkBoxDefault.Checked)
+            {
+                _comboBoxFont.Enabled = false;
+                _checkBoxUnicodeFont.Enabled = false;
+                _labelFontChars.Enabled = false;
+                _textBoxFontChars.Enabled = false;
+            }
+            else
+            {
+                _comboBoxFont.Enabled = true;
+                _checkBoxUnicodeFont.Enabled = true;
+                _labelFontChars.Enabled = true;
+                _textBoxFontChars.Enabled = true;
+            }
+        }
+
+        private void OnCheckBoxUnicodeFontCheckedChanged(object sender, EventArgs e)
+        {
+            if (_checkBoxUnicodeFont.Checked)
+            {
+                _labelFontChars.Enabled = false;
+                _textBoxFontChars.Enabled = false;
+            }
+            else
+            {
+                _labelFontChars.Enabled = true;
+                _textBoxFontChars.Enabled = true;
+            }
+        }
+
+        private void OnComboBoxFontTextChanged(object sender, EventArgs e)
+        {
+            if (DoesFontExist(_comboBoxFont.Text, FontStyle.Regular))
+            {
+                _textBoxFontChars.Font = new Font(_comboBoxFont.Text, _textBoxFontChars.Font.Size);
+                _textBoxFontChars.Text = (string)_textBoxFontChars.Tag;
+            }
+            else
+            {
+                _textBoxFontChars.Text = "";
+            }
+        }
+    }
+}
